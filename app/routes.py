3b088461daf5244bfbee30a3290580b34837f81b
Cameron <cameron.garvie96@googlemail.com>
2020-07-27 03:29:19 +0100

    added config file

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,20 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
+
 def index(): 
-	return "Hello World!"
+	user = {'username': 'Cameron'}
+	posts = [
+		{
+			'author': {'username' : 'John'},
+			'body': 'Beautiful day in London'
+		
+		},
+		{
+			'author' : {'username' : 'Dave'},
+			'body' : 'Great day at the movies'
+		}
+	]
+	return render_template('index.html', title='Home', user=user, posts = posts)
